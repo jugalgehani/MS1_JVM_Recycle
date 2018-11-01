d0040713f6c569cc80595b9b40da2e0327908a78
jugalgehani <36434668+jugalgehani@users.noreply.github.com>
2018-11-01 12:57:02 +0530

    Add files via upload

--- MS1_JVM_Recycle.cmd
+++ MS1_JVM_Recycle.cmd
@@ -1,2 +1,14 @@
-START http://appadmin:43d54f54300826e944952031df5e7daf@10.74.160.158:8081/view/Weblogic_Windows/job/MS1_JVM_Recycle/buildWithParameters?token=secureToken
+cd C:\Weblogic_12c
+START /B C:\Oracle\Middleware\Oracle_Home\user_projects\domains\wl_server\bin\stopManagedWebLogic1.cmd MS1
+timeout /t 20 /nobreak > NUL
+powershell ./MS1_JVM_Stop_Notification.ps1
+cd C:\Weblogic_12c
+set BUILD_ID=doNotKill
+start /B C:\Oracle\Middleware\Oracle_Home\user_projects\domains\wl_server\bin\startManagedWebLogic.cmd MS1
+timeout /t 60 /nobreak > NUL
+cd C:\Oracle\Middleware\Oracle_Home\user_projects\domains\wl_server\servers\MS1\logs
+findstr "RUNNING" MS1.log
+cd C:\Weblogic_12c
+powershell ./MS1_JVM_Start_Notification.ps1
 timeout /t 120 /nobreak > NUL
+EXIT 0
